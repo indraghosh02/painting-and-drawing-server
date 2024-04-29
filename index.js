32f72bb84d6fe4598bf81b07e5eee23ddf003fdc
indraghosh02 <indraghosh0802@gmail.com>
2024-04-29 21:36:34 +0600

    last final commit

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
+require('dotenv').config();
+
 const express = require('express');
 const cors = require('cors');
-require('dotenv').config();
+
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const app = express();
 const port = process.env.PORT || 5000;
+app.use(
+  cors({
+      origin: ['http://localhost:5173', 'https://painting-and-drawing-5bbe7.web.app'],
+      credentials: true,
+  }),
+)
 
+app.use(express.json());
 
 
 
@@ -13,7 +22,7 @@ const port = process.env.PORT || 5000;
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.obyjfl3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
-console.log(uri);
+
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -27,7 +36,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
 
     const artCollection = client.db('artDB').collection('art');
@@ -61,7 +70,7 @@ async function run() {
 
     app.post('/art', async(req, res) =>{
       const newArt = req.body;
-      console.log(newArt);
+      
       const result = await artCollection.insertOne(newArt);
       res.send(result);
     }
@@ -156,7 +165,7 @@ app.get('/artbysubcategory/:subcategory', async (req, res) => {
 
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
@@ -169,8 +178,7 @@ run().catch(console.dir);
 
 //middleware
 
-app.use(cors());
-app.use(express.json());
+
 
 
 app.get('/', (req, res) => {
